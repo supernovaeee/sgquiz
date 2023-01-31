9b597941c63e06a0f95a8e582e4cc3b2d50a6268
supernovaeee <azzasekarnovaa@gmail.com>
2023-01-31 17:42:05 +0800

    Implement session in index.php, initialise session variables, disallow blank spaces for name variable

--- index.php
+++ index.php
@@ -7,8 +7,33 @@
     <title>Document</title>
 </head>
 <body>
-    <?php require_once 'processIndex.php' ?>
-    <form action='processIndex.php' method='POST'>
+<?php
+    if (isset($_POST['submit'])) {
+
+        $name = $_POST['name'];
+        $trimName = trim($name);
+        if (empty($trimName)){
+            $error = "Name cannot be blank";
+            echo "<h4 style='color:red;'>$error</h4>";
+        } else {
+            session_start();
+            $_SESSION['overallScore'] = 0;
+            $_SESSION['name'] = $trimName;
+            $_SESSION['historyAttempt'] = 0;
+            $_SESSION['geoAttempt'] = 0;
+            $_SESSION['wrong'] = 0;
+            $_SESSION['correct'] = 0;
+            $_SESSION['attempted'] = "";
+            if ($_POST['option']=='his'){
+                header('Location: history1.php');
+            }
+            else{
+                header('Location: geo1.php');
+            }
+        }
+    }
+?>
+    <form action='index.php' method='POST'>
         <div><h1>Singapore General Knowledge Quiz</h1></div>
         <div class="name-container">
             <input type="text" name="name" placeholder="Your Name*" required>
