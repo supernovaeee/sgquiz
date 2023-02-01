1741707d13b09659bb9db2e3b6afea5703de52ca
supernovaeee <azzasekarnovaa@gmail.com>
2023-02-02 02:38:37 +0800

    Fix the equality problem: Set two different variables for storing ans key to previous question and live question.

--- game.php
+++ game.php
@@ -13,7 +13,8 @@
     ini_set('display_errors', 1);
     error_reporting(E_ALL ^ E_NOTICE);
     session_start();
-    var_dump($_SESSION);
+    // correctAns variable to store correctAnsLive in previous question
+    $_SESSION['$correctAns'] = $_SESSION['$correctAnsLive'];
     ?>
     <?php
     // Open questions.txt file
@@ -41,7 +42,7 @@
     // Set session variables to store the randomly generated values into global variables
     $_SESSION['$question_ID'] = $randLineArray[0];
     $_SESSION['question'] = $randLineArray[2];
-    $_SESSION['$correctAns'] = $randLineArray[3];
+    $_SESSION['$correctAnsLive'] = $randLineArray[3];
     $_SESSION['$wrongAns1'] = $randLineArray[4];
     $_SESSION['$wrongAns2'] = $randLineArray[5];
     $_SESSION['$wrongAns3'] = $randLineArray[6];
@@ -68,26 +69,26 @@
         } else {
             // 4 types of questions layout: correct Answer can be at 1st, 2nd, 3rd, or 4th position
             $layout1 = '
-            <input type="radio" id="correctAns" name="answer" value="correctAns">
-            <label for="correctAns">' . $_SESSION['$correctAns'] . ' </label><br>' . '<input type="radio" id="wrongAns1" name="answer" value="wrongAns1">
+            <input type="radio" id="correctAnsLive" name="answer" value="correctAnsLive">
+            <label for="correctAnsLive">' . $_SESSION['$correctAnsLive'] . ' </label><br>' . '<input type="radio" id="wrongAns1" name="answer" value="wrongAns1">
             <label for="wrongAns1">' . $_SESSION['$wrongAns1'] . ' </label><br>' . '<input type="radio" id="wrongAns2" name="answer" value="wrongAns2">
             <label for="wrongAns2">' . $_SESSION['$wrongAns2'] . ' </label><br>' . '<input type="radio" id="wrongAns3" name="answer" value="wrongAns3">
             <label for="wrongAns3">' . $_SESSION['$wrongAns3'] . ' </label><br>';
             $layout2 = '<input type="radio" id="wrongAns1" name="answer" value="wrongAns1">
-            <label for="wrongAns1">' . $_SESSION['$wrongAns1'] . ' </label><br>' . '<input type="radio" id="correctAns" name="answer" value="correctAns">
-            <label for="correctAns">' . $_SESSION['$correctAns'] . ' </label><br>' . '<input type="radio" id="wrongAns2" name="answer" value="wrongAns2">
+            <label for="wrongAns1">' . $_SESSION['$wrongAns1'] . ' </label><br>' . '<input type="radio" id="correctAnsLive" name="answer" value="correctAnsLive">
+            <label for="correctAnsLive">' . $_SESSION['$correctAnsLive'] . ' </label><br>' . '<input type="radio" id="wrongAns2" name="answer" value="wrongAns2">
             <label for="wrongAns2">' . $_SESSION['$wrongAns2'] . ' </label><br>' . '<input type="radio" id="wrongAns3" name="answer" value="wrongAns3">
             <label for="wrongAns3">' . $_SESSION['$wrongAns3'] . ' </label><br>';
             $layout3 = '<input type="radio" id="wrongAns1" name="answer" value="wrongAns1">
             <label for="wrongAns1">' . $_SESSION['$wrongAns1'] . ' </label><br>' . '<input type="radio" id="wrongAns2" name="answer" value="wrongAns2">
-            <label for="wrongAns2">' . $_SESSION['$wrongAns2'] . ' </label><br>' . '<input type="radio" id="correctAns" name="answer" value="correctAns">
-            <label for="correctAns">' . $_SESSION['$correctAns'] . ' </label><br>' . '<input type="radio" id="wrongAns3" name="answer" value="wrongAns3">
+            <label for="wrongAns2">' . $_SESSION['$wrongAns2'] . ' </label><br>' . '<input type="radio" id="correctAnsLive" name="answer" value="correctAnsLive">
+            <label for="correctAnsLive">' . $_SESSION['$correctAnsLive'] . ' </label><br>' . '<input type="radio" id="wrongAns3" name="answer" value="wrongAns3">
             <label for="wrongAns3">' . $_SESSION['$wrongAns3'] . ' </label><br>';
             $layout4 = '<input type="radio" id="wrongAns1" name="answer" value="wrongAns1">
             <label for="wrongAns1">' . $_SESSION['$wrongAns1'] . ' </label><br>' . '<input type="radio" id="wrongAns2" name="answer" value="wrongAns2">
             <label for="wrongAns2">' . $_SESSION['$wrongAns2'] . ' </label><br>' . '<input type="radio" id="wrongAns3" name="answer" value="wrongAns3">
-            <label for="wrongAns3">' . $_SESSION['$wrongAns3'] . ' </label><br>' . '<input type="radio" id="correctAns" name="answer" value="correctAns">
-            <label for="correctAns">' . $_SESSION['$correctAns'] . ' </label><br>';
+            <label for="wrongAns3">' . $_SESSION['$wrongAns3'] . ' </label><br>' . '<input type="radio" id="correctAnsLive" name="answer" value="correctAnsLive">
+            <label for="correctAnsLive">' . $_SESSION['$correctAnsLive'] . ' </label><br>';
             // Randomise the question layout
             $randKey2 = rand(1, 4);
             switch ($randKey2) {
@@ -104,39 +105,14 @@
                     echo $layout4;
             }
         }
-        $correctAnsEssay = trim($_SESSION['$correctAns']);
-        echo "Correct Ans from SESSION:" . $correctAnsEssay;
-        echo "<br>";
-        echo "Type: " . gettype($correctAnsEssay);
-        echo "<br>";
-
+        $correctAnsLive = trim($_SESSION['$correctAnsLive']);
+        $correctAns = trim($_SESSION['$correctAns']);
         ?>
         <?php
         if (isset($_POST['forward'])) {
             $userAns = trim($_POST['answer']);
-            echo "Your previous answer: " . $userAns;
-            echo "<br>";
-            echo "Type: " . gettype($userAns);
-            echo "<br>";
-            $userAnsEqualTocorrectAnsEssay = $userAns == $correctAnsEssay;
-            echo "Is boolean?" . is_bool($userAnsEqualTocorrectAnsEssay);
-            echo "<br>";
-            echo "Is userAns equal to correctAnsEssay?";
-            switch ($userAnsEqualTocorrectAnsEssay) {
-                case true:
-                    echo 'True';
-                    break;
-                case false:
-                    echo 'False';
-                    break;
-                case null:
-                    echo 'Null';
-                    break;
-                default:
-                    echo 'Unknown';
-            }
-            echo "<br>";
-            if ($userAns == 'correctAns' || $userAnsEqualTocorrectAnsEssay) {
+            // Compare userAns with correctAnsLive as string (MCQ) or with correctAns variable (short-answer qn)
+            if ($userAns == 'correctAnsLive' || $userAns == $correctAns) {
                 echo "You are correct";
                 echo "<br>";
             } else {
