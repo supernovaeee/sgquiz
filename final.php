d03e437a4a93bd0f6f6a172d5e234c8b839f582d
supernovaeee <azzasekarnovaa@gmail.com>
2023-02-02 16:05:21 +0800

    Implement counter and display point after game ends.

--- final.php
+++ final.php
@@ -1,13 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Your Result</h1>
-    <p>Number of correct =</p>
+    <?php
+    session_start();
+    $correct = $_SESSION['correct'];
+    $wrong = $_SESSION['wrong'];
+    echo "Correct Answer: " . $correct;
+    echo '<br>';
+    echo "Wrong Answer: " . $wrong;
+    echo '<br>';
+    function counter($correct, $wrong)
+    {
+        return $correct * 5 - $wrong * 3;
+    }
+    $point = counter($correct, $wrong);
+    $_SESSION['overallScore'] += $point;
+    echo "Your Point: " . $point;
+    echo '<br>';
+    echo "Your Overall Score: " . $_SESSION['overallScore'];
+
+    ?>
 </body>
+
 </html>
